291b28e4e8809dfacc870f03e65ac4b2f304342a
richbenwell <richbenwell@hotmail.com>
2026-02-08 22:11:04 -0500

    OpenSearch plugin - initial commit

---
+++ plugins/OpenSearch/v1/dataStreams/scripts/search.js
@@ -0,0 +1,32 @@
+if (data.aggregations) {
+  
+  const agg = data.aggregations[Object.keys(data.aggregations).at(-1)]; // last aggregation to support pipelining
+
+  if(agg.buckets) {
+
+    result = agg.buckets;
+  
+  } else if (agg.values) {
+
+    result = [agg.values];
+
+  } else if (agg.value) {
+
+    result = [{
+      value: agg.value
+    }];
+
+  } else {
+
+    result = [agg];
+
+  }
+
+} else {
+  
+  result = data.hits.hits.map(hit => hit._source);
+
+}
+
+
+
