f3e163cacd2cd1148a51cf814c75a94260d132b0
WaynePlummer <wayne.plummer@squaredup.com>
2026-05-14 16:10:56 +0100

    Add import for alert rules

---
+++ plugins/Grafana/v1/dataStreams/scripts/grafanaAlertRulesImport.js
@@ -0,0 +1,20 @@
+const groups = data?.data?.groups || [];
+const rules = [];
+
+for (const group of groups) {
+    for (const rule of group.rules) {
+        if (rule.type !== 'alerting') continue;
+        rules.push({
+            sourceId: `${group.file}~${group.name}~${rule.name}`,
+            name: rule.name,
+            groupName: group.name,
+            namespace: group.file,
+            query: rule.query || '',
+            duration: typeof rule.duration === 'number' ? rule.duration : 0,
+            labels: rule.labels ? JSON.stringify(rule.labels) : '{}',
+            annotations: rule.annotations ? JSON.stringify(rule.annotations) : '{}'
+        });
+    }
+}
+
+result = rules;
