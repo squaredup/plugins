12dcc97201eb52e7e97f90ef6372d2e3339f2cb2
WaynePlummer <wayne.plummer@squaredup.com>
2026-05-21 16:30:44 +0100

    Switch alert rule sourceId to be the uid and then use rawId in format expressions to reference them

--- plugins/Grafana/v1/dataStreams/scripts/grafanaAlertRulesImport.js
+++ plugins/Grafana/v1/dataStreams/scripts/grafanaAlertRulesImport.js
@@ -5,11 +5,10 @@ for (const group of groups) {
     for (const rule of group.rules) {
         if (rule.type !== 'alerting') continue;
         rules.push({
-            sourceId: `${group.file}~${group.name}~${rule.name}`,
+            sourceId: rule.uid,
             name: rule.name,
             groupName: group.name,
             namespace: group.file,
-            ruleUID: rule.uid || '',
             query: rule.query || '',
             duration: typeof rule.duration === 'number' ? rule.duration : 0,
             labels: rule.labels ? JSON.stringify(rule.labels) : '{}',
