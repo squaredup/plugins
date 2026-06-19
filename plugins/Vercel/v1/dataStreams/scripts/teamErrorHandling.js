b12c5f7843361b770bfb84a8c85c8a5dd1773156
Andrew Harris <andrew.harris@squaredup.com>
2026-06-19 13:04:47 +0100

    slight text tweak

--- plugins/Vercel/v1/dataStreams/scripts/teamErrorHandling.js
+++ plugins/Vercel/v1/dataStreams/scripts/teamErrorHandling.js
@@ -1,5 +1,5 @@
 if (response.status === 404) {
     result =
-        "This data stream requires a Team ID. Add your Vercel Team ID in the data source settings to monitor a Team.";
+        "This data stream requires a Team ID. Add your Vercel Team ID in the data source settings to monitor Teams.";
 }
 
