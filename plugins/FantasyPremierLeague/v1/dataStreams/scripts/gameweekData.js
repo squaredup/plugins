e8d5bd6699cb0523b9c66367083b734ec651f0f6
Tim Wheeler <63284593+TimWheeler-SQUP@users.noreply.github.com>
2026-08-18 17:38:01 +0100

    Revert stray v1 changes superseded by v2
    v1's import/config-validation additions predate the move to v2 and
    were left behind. v2 is the sole carrier of import support; v1 stays
    published as-is on main.

--- plugins/FantasyPremierLeague/v1/dataStreams/scripts/gameweekData.js
+++
@@ -1,7 +0,0 @@
-result = data.events.map(function (event) {
-    var chipMap = {};
-    (event.chip_plays || []).forEach(function (chip) {
-        chipMap[chip.chip_name] = chip.num_played;
-    });
-    return Object.assign({}, event, { chip_plays: chipMap });
-});
